53d2a3d7d214e2c00da01002ac4e78bd5a1f004f
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-04-10 10:57:41 -0600

    utils folder added

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -27,34 +55,6 @@ const ThoughtSchema = new Schema(
       getters: true,
     },
     id: false,
-  }
-);
-
-const ReactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
   }
 );
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,14 @@
 // Definitely need help with this one
 
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./users-routes');
+const thoughtRoutes = require('./thoughts-routes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
-router.use((req, res) => {
-  res.status(404).send('<h1>😝 404 Error!</h1>');
-});
+// router.use((req, res) => {
+//   res.status(404).send('<h1>😝 404 Error!</h1>');
+// });
 
 module.exports = router;
